52a38a191c3fe9dadd62bda56cbdcadb4fe02ce1
Daniel West <daniel@fedora.jpi>
2013-02-05 14:35:15 -0500

    Creating the general structure for the accumulo addition based on hBase

---
+++ titan-accumulo/src/main/java/com/thinkaurelius/titan/diskstorage/accumulo/AccumuloTransaction.java
@@ -0,0 +1,19 @@
+package com.thinkaurelius.titan.diskstorage.accumulo;
+
+import com.thinkaurelius.titan.diskstorage.common.AbstractStoreTransaction;
+import com.thinkaurelius.titan.diskstorage.keycolumnvalue.ConsistencyLevel;
+
+/**
+ * This class overrides and adds nothing compared with
+ * {@link com.thinkaurelius.titan.diskstorage.locking.consistentkey.ConsistentKeyLockTransaction}; however, it creates a transaction type specific
+ * to Accumulo, which lets us check for user errors like passing a Cassandra
+ * transaction into a Accumulo method.
+ *
+ * @author Dan LaRocque <dalaro@hopcount.org>
+ */
+public class AccumuloTransaction extends AbstractStoreTransaction {
+
+    public AccumuloTransaction(ConsistencyLevel level) {
+        super(ConsistencyLevel.KEY_CONSISTENT);
+    }
+}
